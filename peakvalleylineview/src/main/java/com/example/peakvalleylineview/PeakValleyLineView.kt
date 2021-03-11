b31811c7b99b73eb9f4d95422984ffd52237f07f
Anwesh43 <anweshthecool0@gmail.com>
2021-03-12 00:45:08 +0530

    adding Canvas extension methods

--- peakvalleylineview/src/main/java/com/example/peakvalleylineview/PeakValleyLineView.kt
+++ peakvalleylineview/src/main/java/com/example/peakvalleylineview/PeakValleyLineView.kt
@@ -28,3 +28,27 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawPeakValleyLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = w / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val y : Float = h * 0.5f
+    save()
+    translate(w, 0f)
+    drawLine(0f, 0f, -size * sf1, y * sf1, paint)
+    drawLine(-size, y, -size - (w - 2 * size) * sf2, y, paint)
+    drawLine(-w + size, y, -w + size - size * sf3, y * (1 + sf3), paint)
+    restore()
+}
+
+fun Canvas.drawPVLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawPeakValleyLine(scale, w, h, paint)
+}
