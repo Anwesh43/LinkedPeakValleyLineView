856fbe3c0b896326f6b3e9f33d2feb9b267e3499
Anwesh43 <anweshthecool0@gmail.com>
2021-03-12 00:30:40 +0530

    adding Int and Float extension methods

--- peakvalleylineview/src/main/java/com/example/peakvalleylineview/PeakValleyLineView.kt
+++ peakvalleylineview/src/main/java/com/example/peakvalleylineview/PeakValleyLineView.kt
@@ -23,3 +23,8 @@ val strokeFactor : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val parts : Int = 3
 val scGap : Float = 0.02f / parts
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
