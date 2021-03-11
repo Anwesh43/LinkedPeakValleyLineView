940540e7b5ba354b3c231dd4c07ebf1a94b4cc31
Anwesh43 <anweshthecool0@gmail.com>
2021-03-12 01:29:12 +0530

    adding renderer in view

--- peakvalleylineview/src/main/java/com/example/peakvalleylineview/PeakValleyLineView.kt
+++ peakvalleylineview/src/main/java/com/example/peakvalleylineview/PeakValleyLineView.kt
@@ -55,14 +55,16 @@ fun Canvas.drawPVLNode(i : Int, scale : Float, paint : Paint) {
 
 class PeakValleyLineView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer  : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
